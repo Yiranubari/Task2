a533a888a82e8cdcaa95cbdde5ad0d8dc0cd069f
Yiranubari <yiranubari4@gmail.com>
2025-10-27 15:45:51 +0100

    FIX: Add .htaccess for URL rewriting and route handling

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ date_default_timezone_set('UTC');
 
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 $requestPath = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/';
+$requestPath = isset($_GET['route']) ? $_GET['route'] : '/';
 
 switch ($requestMethod) {
     case 'POST':
